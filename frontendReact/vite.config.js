208fe5d85e9f5320687e76714e522e07627aca60
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2025-03-16 19:26:06 -0700

    fixed css

--- frontendReact/vite.config.js
+++ frontendReact/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['c601-2601-647-4b00-eb00-3c31-561d-af62-b1c0.ngrok-free.app'], // Allow this host
+  },
 })
